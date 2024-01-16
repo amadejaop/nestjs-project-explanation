aa9e7dfb712b5e367207f052cd5b5398d67fcd46
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-01-16 17:54:29 +0100

    Create and add AuthCredentialsDto
    Created and added AuthCredentialsDto to service and controller as well as user repository.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthCredentialsDto } from './auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService: AuthService
+    ) { }
+
+    @Post('/signup')
+    signUp(@Body() authcredentials: AuthCredentialsDto): Promise<void> {
+        return this.authService.signUp(authcredentials);
+    }
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,12 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./auth-credentials.dto";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+        const user = this.create({ username, password });
+        await this.save(user);
+    }
 }
