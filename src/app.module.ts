74b5ecfd0c3cca4441eb57e97f9c066333b75af8
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-01-16 15:23:50 +0100

    Add typeorm config file
    Created and added typeorm config file to the app.module.ts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule
+  ],
 })
 export class AppModule {}
