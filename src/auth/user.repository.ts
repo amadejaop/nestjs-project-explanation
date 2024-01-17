f1192adbcca3f195a7f8a3e4c788456c7dba05e0
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-01-17 05:32:18 +0100

    Modify code to accept unique username
    Modified user entity and repository to only accept unique usernames upon registration. Added a try...catch block to catch the error when using the same username that already exists inside the database and throw an appropriate error message.

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,26 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./auth-credentials.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password });
-        await this.save(user);
+        
+        try {
+            await this.save(user);
+    
+        } catch (error) {
+            // if the error code corresponds to the one given when username already exists
+            // console.log(error) to get the error code
+            if (error.code === "23505") {
+                throw new ConflictException("Username already exists.");
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+
     }
 }
