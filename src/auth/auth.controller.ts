fcdcf89903e451ffad587aaa82a4cf7a5c8e5761
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-01-17 06:56:26 +0100

    Implement getting user info after auth
    Check if token is valid, if it is get user information

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +17,11 @@ export class AuthController {
     @Post('signin')
     signIn(@Body(ValidationPipe) authCredentials: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentials);
+    }
+
+    @Post('/test')
+    @UseGuards(AuthGuard())
+    authTest(@Req() req) {
+        console.log('req', req)
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PassportModule } from '@nestjs/passport';
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [PassportModule, JwtStrategy]
 })
 export class AuthModule {}
