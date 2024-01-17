324aa19a21b70268368f2b2837552708df38e3df
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-01-17 07:14:57 +0100

    Add relationship between user and board

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Board } from "src/boards/board.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
 @Unique(['username'])
@@ -11,4 +12,8 @@ export class User extends BaseEntity {
 
     @Column()
     password: string;
+
+    @OneToMany(type => Board, board => board.user, {eager: true})
+    // multiple boards, hence the array
+    boards: Board[];
 }
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,13 @@ import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([BoardRepository])
+  ],
   controllers: [BoardsController],
   providers: [BoardsService]
 })
